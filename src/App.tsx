c1d2340dddaddfd78463336b6d4219525e3a1c07
tanipu <67771199+yuktnk@users.noreply.github.com>
2021-11-06 18:44:16 +0900

    json placeholderからデータ取得、ローディング処理、エラー処理

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
+import axios from "axios";
+import { UserCard } from "./components/UserCard";
+import { User } from "./types/api/user";
+
 import "./styles.css";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFeechUser = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Stssart editing to see some magic happen!</h2>
+      <button onClick={onClickFeechUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました。</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
